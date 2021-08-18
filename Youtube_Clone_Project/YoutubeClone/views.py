3d0194562c1d3f854303ff8b203725a01f362e7a
Paul-Longo <imanazarite88@gmail.com>
2021-08-18 17:25:22 -0400

    updated CommentsList class with a post method.

--- Youtube_Clone_Project/YoutubeClone/views.py
+++ Youtube_Clone_Project/YoutubeClone/views.py
@@ -3,7 +3,7 @@ from .models import Comments
 from .serializers import CommentsSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import serializers, status
+from rest_framework import status
 
 # Create your views here.
 
@@ -14,3 +14,10 @@ class CommentsList(APIView):
         comment = Comments.objects.all()
         serializer = CommentsSerializer(comment, many=True)
         return Response(serializer.data)
+
+    def post (self, request):
+        serializer = CommentsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
