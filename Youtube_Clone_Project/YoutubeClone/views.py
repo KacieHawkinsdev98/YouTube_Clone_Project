2e66504e557ebd07f10a42cb96ed01d8c8f59a23
Paul-Longo <imanazarite88@gmail.com>
2021-08-18 16:48:45 -0400

    started a views.py added and added a CommentsList(APIView) class.

--- Youtube_Clone_Project/YoutubeClone/views.py
+++ Youtube_Clone_Project/YoutubeClone/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Comments
+from .serializers import CommentsSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import serializers, status
 
 # Create your views here.
+
+
+class CommentsList(APIView):
+
+    def get(self, request):
+        comment = Comments.objects.all()
+        serializer = CommentsSerializer(comment, many=True)
+        return Response(serializer.data)
