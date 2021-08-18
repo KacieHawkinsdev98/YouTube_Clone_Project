3a4e28d39d2cebc06d15782e3823292b530cee27
Paul-Longo <imanazarite88@gmail.com>
2021-08-18 17:07:35 -0400

    updated videoID to video_id

--- Youtube_Clone_Project/YoutubeClone/serializers.py
+++ Youtube_Clone_Project/YoutubeClone/serializers.py
@@ -2,7 +2,8 @@ from django.db.models import fields
 from rest_framework import serializers
 from .models import Comments
 
+
 class CommentsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comments
-        fields = ['videoID', 'comment', 'replies', 'likes', 'dislikes']
+        fields = ['video_id', 'comment', 'replies', 'likes', 'dislikes']
