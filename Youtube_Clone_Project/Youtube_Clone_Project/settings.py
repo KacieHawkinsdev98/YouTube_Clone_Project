68bb4f053ce6deae68a026692e22bb5aa3afeee0
unknown <kaciehawkins1@gmail.com>
2021-08-19 15:26:33 -0600

    added corsheaders to installed apps and middleware

--- Youtube_Clone_Project/Youtube_Clone_Project/settings.py
+++ Youtube_Clone_Project/Youtube_Clone_Project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'YoutubeClone'
+    'YoutubeClone',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
+
+COR_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'Youtube_Clone_Project.urls'
 
